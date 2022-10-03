a70d85ca04e29c03fc8edce2924721a74cdecae5
koechronix <koechronix@yahoo.com>
2022-10-03 23:02:30 +0300

    Added seeding and did migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding restaurants..."
+Restaurant.create!([
+  {
+    name: "Sottocasa NYC",
+    address: "298 Atlantic Ave, Brooklyn, NY 11201"
+  },
+  {
+    name: "PizzArte",
+    address: "69 W 55th St, New York, NY 10019"
+  },
+  {
+    name: "San Matteo NYC",
+    address: "1559 2nd Ave, New York, NY 10028"
+  }
+])
+
+puts "Seeding pizzas..."
+Pizza.create!([
+  {
+    name: "Cheese",
+    ingredients: "Dough, Tomato Sauce, Cheese"
+  },
+  {
+    name: "Pepperoni",
+    ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni"
+  },
+  {
+    name: "Pillau",
+    ingredients: "Meat, Rice, Tomato, Chilly"
+  },
+  {
+    name: "Chicken",
+    ingredients: "Cheese, Tomato, Chilly, Pilipilihoho"
+  },
+  {
+    name: "Chapoo",
+    ingredients: "Soup, cheese, meat"
+  }
+])
+
+puts "Adding pizzas to restaurants..."
+Restaurant.all.each do |restaurant|
+  rand(1..3).times do
+    # get a random pizza
+    pizza = Pizza.find(Pizza.pluck(:id).sample)
+
+    RestaurantPizza.create!(restaurant_id: restaurant.id, pizza_id: pizza.id, price: rand(3..28))
+  end
+end
+
+puts "Done seeding!"
