cc4f599b32b44ce9caffcc29953d2f4b4a4956a0
koechronix <koechronix@yahoo.com>
2022-10-03 23:19:49 +0300

    Added validation to restaurant_pizza

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,6 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :restaurant 
     belongs_to :pizza 
+
+    validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
 end
