bda36048e89ef14d35bd00a47505d91142590854
koechronix <koechronix@yahoo.com>
2022-10-03 23:37:09 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :restaurants, only: [:index, :show, :destroy]
+  resources :pizzas, only: [:index]
+  resources :restaurant_pizzas, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
